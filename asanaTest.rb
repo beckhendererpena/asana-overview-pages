15194ecc601a6896c3de836998415ecb96b2caed
Beck Henderer-Peña <theotherbeck@gmail.com>
2013-12-09 12:32:22 -0800

    added ability to get tasks by tag, and list needed data there
    next step is to put that same info into HAML

--- asanaTest.rb
+++ asanaTest.rb
@@ -5,13 +5,16 @@ Asana.configure do |client|
 end
 
 #get workspaces from Asana
-workspaces = Asana::Workspace.all
+# workspaces = Asana::Workspace.all
+
+#asana-visualization project id
+asanaProject = Asana::Project.find("8820311263807")
 
 # Get all projects
 projects = Asana::Project.all
 
 # Get all projects in a given workspace
-workspace = Asana::Workspace.find("593447651003")
+# workspace = Asana::Workspace.find("593447651003")
 # projects = workspace.projects
 
 #holds all active projects, defined by color dark-green
@@ -28,16 +31,35 @@ end
 #to get detailed project info, you have to use the project ID and look it up on it's own.  When doing it through workspaces, 
 #it just returns id and name
 
+def getSingleProject(proj, tag1, tag2)
+  tasks = proj.tasks
+  tasks.each do |t|
+    t.tags.each do |tags|
+      if tags.name == tag1
+        puts t.name
+        puts t.notes
+        puts t.due_on
+      end
+      if tags.name == tag2
+        puts t.name
+        puts t.assignee.name
+        puts t.notes
+        #eventually add comments?
+      end
+    end
+  end
+end
+
 def getActiveProjects(proj, active)
   proj.each do |p|
-    projectName = p.name
-    projectId = p.id
+    # projectName = p.name
+    # projectId = p.id
     project = Asana::Project.find(projectId)
       #this should get out of this function, and then uncomment out the one below it
       if project.color == "dark-green"
         tasks = project.tasks
         tasks.each do |t|
-          puts t.name #now only do this if it's tagged properly
+          puts t.name #now only do this if it's tagged properly - each of these loops goes in the HAML - anything with PUTS
         end
       end
       # if project.color == "dark-green"
@@ -52,13 +74,13 @@ end
 
 #run ----->
 
-puts "Workspace:" 
+# puts "Workspace:" 
 #print workspace name
-printWorkspaceName(workspace)
+# printWorkspaceName(workspace)
 
 #print all project names to the screen
 getActiveProjects(projects, activeProjects)
 
-
+getSingleProject(asanaProject, "MILESTONE", "overview")
 
 
