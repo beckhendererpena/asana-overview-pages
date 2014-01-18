6515a495d2bc1610e0c27dbc721547f339fc55f3
Beck Henderer-Peña <theotherbeck@gmail.com>
2014-01-17 17:46:58 -0800

    comments collected but in their own field
    need to merge them into tasks somehow

--- website_personal.rb
+++ website_personal.rb
@@ -71,6 +71,7 @@ get '/Milestones' do
   active_projects = all_projects["data"].select { |e| e["color"] == "dark-green" }
 
   @active_project_data = []
+  @test_array = []
 
   active_projects.each do |e| 
 
@@ -87,6 +88,7 @@ get '/Milestones' do
 
     #use an if to check to see if this user has any tasks assigned to them in this project.  If they do, then do this push.
     filtered_tasks = []
+    collected_comments = []
 
     if milestones.any?
       
@@ -95,12 +97,24 @@ get '/Milestones' do
 
       #get tag info for each task
       task_id.each do |id|
-        tag_info = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/tasks/" + id + "/tags", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body) #try an option_expand here from Asana
+        tag_info = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/tasks/" + id + "/tags", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body) #try an option_expand here from Asana  #returns an hash "data" with it's value as an array
 
         #check the name of the tag
         if tag_info["data"][0]["name"] == "MILESTONE"   #this is limited... will not work if task has multiple tags - bleh.
           #if it's the one we want, put it in the filtered_tasks array
-          filtered_tasks.push(milestones.find { |task| task["id"] == id.to_i}) 
+          currentTask = milestones.find { |task| task["id"] == id.to_i} #returns task object, whatever that is
+
+          stories = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/tasks/" + id + "/stories?opt_fields=type,text", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body) #returns data array with hashes in each index, one for each comment.  "created by" key has hash as value, and includes "id" and "name"   
+
+          if stories.any?  
+          stories["data"].each do |story|
+            if story["type"] == "comment"
+              collected_comments.push(story)
+            end
+          end
+        end
+
+          filtered_tasks.push(currentTask) #push the task to the filtered array
         end
 
         # or like this:   filtered_tasks.push(milestones.find { |task| task["id"] == id.to_i})  if tag_info["data"][0]["name"] == "MILESTONE"
@@ -108,18 +122,17 @@ get '/Milestones' do
         #get followers ids - there are multiple ids possible.  figure that out.
         #check for comments
 
-        # comments = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/tasks/" + id + "/stories", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body) #returns data array with hashes in each index, one for each comment.  "created by" key has hash as value, and includes "id" and "name"
+        
 
-        # if comments.any?
-        #   comments.each do |comment|
-        #     #put whole comment hash into the task info
-        #   end
-        # end
+        
 
       end
 
       #put that data into an array, for looping through in HAML
       project["tasks"] = filtered_tasks   #.sort_by! {| task | task["due_on"] } 
+      project["comments"] = collected_comments  #project["tasks"]["comments"]  
+
+      #works!!!!  now just have to get it inside task array.  Merge them somehow?
       @active_project_data.push(project)
     end
 
