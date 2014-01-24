56b8c199a35aa83ede22865bfba751b377a60a86
Beck Henderer-Peña <theotherbeck@gmail.com>
2014-01-23 17:04:25 -0800

    cleaned up code
    hope I didn't break anything!

--- asana-task-display.rb
+++ asana-task-display.rb
@@ -25,56 +25,13 @@ end
 
 ######################################################   Routes
 
-get '/User/:id' do 
-  
-  user_id = params[:id].to_i
-  all_projects = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/projects/?opt_fields=color,name", userpwd: $key).body)
-
-  active_projects = all_projects["data"].select { |e| e["color"] == "dark-green" }
-
-  @active_project_data = []
-
-  active_projects.each do |e| 
-
-    #make a new hash to store project data in
-    project = Hash.new
-    project["name"] = e["name"]
-    project["id"] = e["id"]
-    all_tasks = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/projects/" + e["id"].to_s + "/tasks?opt_fields=name,notes,due_on,assignee,completed,tags&opt_expand=name", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body)
-    
-    #parse the full list of tasks to see if there are any for the user
-    tasksForUser = all_tasks["data"].select { |task| task["assignee"] != nil && task["completed"] == false && task["assignee"]["id"] == user_id}
-
-    #check to see if this user has any tasks assigned to them in this project.  If they do, then do this push.
-    if tasksForUser.any?
-      #put that data into an array, for looping through in HAML
-      project["tasks"] = tasksForUser       #.sort_by! {| task | task["due_on"] }    #.reverse!  
-      @active_project_data.push(project)
-    end
-
-
-
-          #gets stories for comments - I don't think we'll use this in this Milestones version, but will use for individual project pages.
-
-          # if stories.any?  
-          #   stories["data"].each do |story|
-          #     if story["type"] == "comment"
-          #       collected_comments.push(story)
-          #     end
-          #   end
-          # end
-    #should I add subtasks?  Can I?
-  end
-
-  haml :personal, :layout => false
-end  
-
-
+#input page
 get '/' do
   
   haml :input, :layout => false
 end
 
+#get parameters for the app
 post '/' do
 
   $key = params[:key]
@@ -82,27 +39,30 @@ post '/' do
   redirect ('/overview')
 end
 
-
+#show the overview
 get '/overview' do 
   
   all_projects = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/projects/?opt_fields=color,name", userpwd: $key).body)
 
   active_projects = all_projects["data"].select { |e| e["color"] == "dark-green" }
 
-  @active_project_data = [] #an array to be filled with hashes for each project
+  @active_project_data = [] #to be filled with hashes for each project
 
-  $asana_tag = $tag #will make this a user input option later
+  $asana_tag = $tag #user selected tag
 
+  #sort out data for each project
   active_projects.each do |e| 
 
     #make a new hash to store project data in
     project = Hash.new
     project["name"] = e["name"]
     project["id"] = e["id"]
+
+    #get all the task data, within parameters of Asana API
     all_tasks = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/projects/" + e["id"].to_s + "/tasks?opt_fields=name,notes,followers,due_on,assignee,tags,completed", userpwd: $key).body)
     
-    #parse the list for milestones
-    tags = all_tasks["data"].select { |task| task["tags"].length >= 1 && task["completed"] == false} 
+    #parse the list for tasks with tags
+    tagged_tasks = all_tasks["data"].select { |task| task["tags"].length >= 1 && task["completed"] == false} 
 
     #show comments too??  or just update in notes?
 
@@ -111,10 +71,10 @@ get '/overview' do
     collected_comments = []
     project_has_tags = false
 
-    if tags.any?
+    if tagged_tasks.any?
       
-      #strip out all the task ids
-      task_id = tags.map { |task| task["id"].to_s }
+      #strip out all the tagged task's ids
+      task_id = tagged_tasks.map { |task| task["id"].to_s }
 
       #get tag info for each task
       task_id.each do |id|
@@ -123,11 +83,7 @@ get '/overview' do
         #check the name of the tag
         if tag_info["data"].any? {|tag| tag["name"] == $asana_tag}   
 
-          
-
-          #if it's the one we want, put it in the filtered_tasks array #would any? work here? "data"].any? {|tag|["name"] == "MILESTONE"} 
-
-          currentTask = tags.find { |task| task["id"] == id.to_i} #returns task object, a hash
+          currentTask = tagged_tasks.find { |task| task["id"] == id.to_i} #returns task object, a hash
 
           stories = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/tasks/" + id + "/stories?opt_fields=type,text", userpwd: $key).body) #returns data array with hashes in each index, one for each comment.  "created by" key has hash as value, and includes "id" and "name"   
 
@@ -139,13 +95,13 @@ get '/overview' do
           currentTaskArray = []
           currentTaskArray.push(currentTask)
 
-
-          #gets followers into task
+          #gets followers into task for "team" list
           followers = [] #to get filled with strings
           get_followers_from_tasks(currentTaskArray, followers) #returns a bunch of strings and puts them in an array
           currentTask["follower_names"] = followers 
 
-          filtered_tasks.push(currentTask) #push the task to the filtered array
+          #put tasks in the filtered_tasks array
+          filtered_tasks.push(currentTask) 
 
           #set some kind of flag that says this project actually has a task in a project with a tag you asked for, and should display that project. If not, don't push in this project...
         end  
@@ -154,8 +110,9 @@ get '/overview' do
 
       if filtered_tasks.any?
         #put that data into an array, for looping through in HAML
-        project["tasks"] = filtered_tasks   #.sort_by! {| task | task["due_on"] } 
+        project["tasks"] = filtered_tasks   #.sort_by! {| task | task["due_on"] } ------ would be nice to have later, but needs to deal with tasks without dates.
 
+        #since this project has some tasks with the tag we want, include that project title in the list of projects to be displayed.
         @active_project_data.push(project)
       end
       
@@ -163,45 +120,6 @@ get '/overview' do
 
   end
 
-
-  haml :personal, :layout => false
-end  
-
-
-get '/test' do
-
-  haml :test, :layout => false
-end
-
-get '/John' do 
-  
-  user_id = 5357621858433
-  all_projects = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/projects/?opt_fields=color,name", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body)
-
-  active_projects = all_projects["data"].select { |e| e["color"] == "dark-green" }
-
-  @active_project_data = []
-
-  active_projects.each do |e| 
-
-    #make a new hash to store project data in
-    project = Hash.new
-    project["name"] = e["name"]
-    project["id"] = e["id"]
-    all_tasks = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/projects/" + e["id"].to_s + "/tasks?opt_fields=name,notes,due_on,assignee,completed,tags", userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body)
-    
-    #parse the full list of tasks to see if there are any for the user
-    tasksForUser = all_tasks["data"].select { |task| task["assignee"] != nil && task["completed"] == false && task["assignee"]["id"] == user_id}
-
-    #check to see if this user has any tasks assigned to them in this project.  If they do, then do this push.
-    if tasksForUser.any?
-      #put that data into an array, for looping through in HAML
-      project["tasks"] = tasksForUser.sort_by! {| task | task["due_on"] }    #.reverse!  
-      @active_project_data.push(project)
-    end
-
-    #should we add subtasks?  Can we?
-  end
 
   haml :personal, :layout => false
 end  
