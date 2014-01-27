755c8a28d5988f0bbade5d72a173dd3e9fe6142d
Beck Henderer-Peña <theotherbeck@gmail.com>
2014-01-27 14:49:49 -0800

    replaced asana key

--- asana-task-display.rb
+++ asana-task-display.rb
@@ -19,7 +19,7 @@ def get_followers_from_tasks(tasks, array) #tasks == an array, array == an array
   tasks.each do |task|
     task["followers"].each do |follower|
       #get user name based on user id
-      user = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/users/" + follower["id"].to_s, userpwd: "4tuQrdX.5djpapCXlKooicNrUgx0zbeY:").body)
+      user = JSON.parse(Typhoeus::Request.get("https://app.asana.com/api/1.0/users/" + follower["id"].to_s, userpwd: $key).body)
       array.push(user["data"]["name"])
     end
   end
