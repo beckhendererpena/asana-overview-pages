b3d70bb44ed75a232ec0273be178d1b9d7910f42
Beck Henderer-Peña <theotherbeck@gmail.com>
2014-01-31 17:30:46 -0800

    oauth?

---
+++ oauth-examples-master/ruby/app.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'omniauth-asana'
+require 'ostruct'
+
+set :port, ENV['PORT']
+set :public_folder, "../public/"
+
+use Rack::Session::Cookie
+use OmniAuth::Builder do
+  provider :asana, ENV['ASANA_CLIENT_ID'], ENV['ASANA_CLIENT_SECRET']
+end
+
+get '/' do
+  if session[:auth]
+    <<-HTML
+    Welcome, #{session[:user][:name]}!
+    Your token is #{session[:auth].token}, your id is #{session[:uid]}
+    <a href='/logout'>Logout</a>
+    HTML
+  else
+    <<-HTML
+    <p>Sinatra demo app for Asana OAuth</p>
+    <a href='/auth/asana'><img src="/asana-oauth-button.png"</a>
+    HTML
+  end
+end
+
+get '/auth/:name/callback' do
+  auth = request.env['omniauth.auth']
+  session[:auth] = auth.credentials
+  session[:uid] = auth.uid
+  session[:user] = auth.info
+  redirect '/'
+end
+
+get '/auth/failure' do
+  raise StandardError, params
+end
+
+get '/logout' do
+  session.destroy
+  redirect '/'
+end
